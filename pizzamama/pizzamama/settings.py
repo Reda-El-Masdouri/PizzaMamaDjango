1dc7909d3b1b94180cd7d4f0119ad7dc474378be
Reda E <reda.elmasdouri@gmail.com>
2022-05-18 23:21:54 +0200

    heroku app

--- pizzamama/pizzamama/settings.py
+++ pizzamama/pizzamama/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-t^3&lblc55^iqp!rxxv9w#r^+*@)g(-ae95xgat)qo%0o=taz5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["RedaElMasdouri.pythonanywhere.com", "127.0.0.1", "pizzamamadjango-rem.herokuapp.com"]
 
 
 # Application definition
@@ -117,10 +118,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+django_heroku.settings(locals())
