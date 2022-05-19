19795461905e0cf0d7140a65963e66b34f52e487
Reda E <reda.elmasdouri@gmail.com>
2022-05-19 20:33:09 +0200

    main.py deskto app to affich information from web site

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello !")
+from webbrowser import get
+import requests
+import json
+
+url = 'https://limitless-harbor-83714.herokuapp.com/api/GetPizza'
+data = requests.get(url)
+#print(data.text)
+
+pizzas = json.loads(data.text)
+
+for pizzamodel in pizzas:
+    pizza = pizzamodel['fields']
+    print(pizza['nom'])
